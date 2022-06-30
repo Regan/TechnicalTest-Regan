ef040df6c02ad1ce052251546a61c1f6e3496002
Regan <46687043+Regan@users.noreply.github.com>
2022-06-30 11:46:09 +0100

    added functionality for ProcessTriangle.

--- TechnicalTest.Core/Services/ShapeService.cs
+++ TechnicalTest.Core/Services/ShapeService.cs
@@ -7,13 +7,27 @@ namespace TechnicalTest.Core.Services
     {
         public Shape ProcessTriangle(Grid grid, GridValue gridValue)
         {
-            // TODO: Calculate the coordinates.
-            return new Shape(new List<Coordinate>
+            Coordinate topLeftVertex, outerVertex, bottomRightVertex;
+            const int adjustPosition = 1;
+            if (gridValue.Column % 2 == 0) // checks if column is even, if so is the top-right triangle.
             {
-                new(0, 0),
-                new(0, 0),
-                new(0, 0)
-            });
+                if (gridValue.Column == 10) // number 10 needs to use a different formula.
+                { 
+                    topLeftVertex = new Coordinate(((gridValue.Column / 2)) * grid.Size, (gridValue.GetNumericRow() - adjustPosition) * grid.Size);
+                    outerVertex = new Coordinate((gridValue.Column / 2) * grid.Size, gridValue.GetNumericRow() * grid.Size);
+                    bottomRightVertex = new Coordinate((gridValue.Column / 2 + adjustPosition) * grid.Size, gridValue.GetNumericRow() * grid.Size);
+                    return new Triangle(topLeftVertex, outerVertex, bottomRightVertex);
+                }
+                topLeftVertex = new Coordinate(((gridValue.Column / 2) - adjustPosition) * grid.Size, (gridValue.GetNumericRow() - adjustPosition) * grid.Size);
+                outerVertex = new Coordinate((gridValue.Column / 2) * grid.Size, (gridValue.GetNumericRow() - 1) * grid.Size);
+                bottomRightVertex = new Coordinate((gridValue.Column / 2) * grid.Size, gridValue.GetNumericRow() * grid.Size);
+                return new Triangle(topLeftVertex, outerVertex, bottomRightVertex);
+            }
+            // bottom left triangle formula
+            topLeftVertex = new Coordinate(gridValue.Column / 2 * grid.Size, (gridValue.GetNumericRow() - adjustPosition) * grid.Size); 
+            outerVertex = new Coordinate(gridValue.Column / 2 * grid.Size, gridValue.GetNumericRow() * grid.Size); 
+            bottomRightVertex = new Coordinate((gridValue.Column / 2 + adjustPosition) * grid.Size, gridValue.GetNumericRow() * grid.Size);
+            return new Triangle(topLeftVertex, outerVertex, bottomRightVertex);
         }
 
         public GridValue ProcessGridValueFromTriangularShape(Grid grid, Triangle triangle)
