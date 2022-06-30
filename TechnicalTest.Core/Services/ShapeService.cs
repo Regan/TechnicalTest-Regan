a98f5bc6ef1ba9d5111838c7428183ad3dacf211
Regan <46687043+Regan@users.noreply.github.com>
2022-06-30 13:12:11 +0100

    added functionality for ProcessGridValue, improved indentation.

--- TechnicalTest.Core/Services/ShapeService.cs
+++ TechnicalTest.Core/Services/ShapeService.cs
@@ -12,28 +12,68 @@ namespace TechnicalTest.Core.Services
             if (gridValue.Column % 2 == 0) // checks if column is even, if so is the top-right triangle.
             {
                 if (gridValue.Column == 10) // number 10 needs to use a different formula.
-                { 
-                    topLeftVertex = new Coordinate(((gridValue.Column / 2)) * grid.Size, (gridValue.GetNumericRow() - adjustPosition) * grid.Size);
-                    outerVertex = new Coordinate((gridValue.Column / 2) * grid.Size, gridValue.GetNumericRow() * grid.Size);
-                    bottomRightVertex = new Coordinate((gridValue.Column / 2 + adjustPosition) * grid.Size, gridValue.GetNumericRow() * grid.Size);
+                {
+                    topLeftVertex = new Coordinate(((gridValue.Column / 2)) * grid.Size,
+                        (gridValue.GetNumericRow() - adjustPosition) * grid.Size);
+                    outerVertex = new Coordinate((gridValue.Column / 2) * grid.Size,
+                        gridValue.GetNumericRow() * grid.Size);
+                    bottomRightVertex = new Coordinate((gridValue.Column / 2 + adjustPosition) * grid.Size,
+                        gridValue.GetNumericRow() * grid.Size);
                     return new Triangle(topLeftVertex, outerVertex, bottomRightVertex);
                 }
-                topLeftVertex = new Coordinate(((gridValue.Column / 2) - adjustPosition) * grid.Size, (gridValue.GetNumericRow() - adjustPosition) * grid.Size);
-                outerVertex = new Coordinate((gridValue.Column / 2) * grid.Size, (gridValue.GetNumericRow() - 1) * grid.Size);
-                bottomRightVertex = new Coordinate((gridValue.Column / 2) * grid.Size, gridValue.GetNumericRow() * grid.Size);
+
+                topLeftVertex = new Coordinate(((gridValue.Column / 2) - adjustPosition) * grid.Size,
+                    (gridValue.GetNumericRow() - adjustPosition) * grid.Size);
+                outerVertex = new Coordinate((gridValue.Column / 2) * grid.Size,
+                    (gridValue.GetNumericRow() - 1) * grid.Size);
+                bottomRightVertex = new Coordinate((gridValue.Column / 2) * grid.Size,
+                    gridValue.GetNumericRow() * grid.Size);
                 return new Triangle(topLeftVertex, outerVertex, bottomRightVertex);
             }
+
             // bottom left triangle formula
-            topLeftVertex = new Coordinate(gridValue.Column / 2 * grid.Size, (gridValue.GetNumericRow() - adjustPosition) * grid.Size); 
-            outerVertex = new Coordinate(gridValue.Column / 2 * grid.Size, gridValue.GetNumericRow() * grid.Size); 
-            bottomRightVertex = new Coordinate((gridValue.Column / 2 + adjustPosition) * grid.Size, gridValue.GetNumericRow() * grid.Size);
+            topLeftVertex = new Coordinate(gridValue.Column / 2 * grid.Size,
+                (gridValue.GetNumericRow() - adjustPosition) * grid.Size);
+            outerVertex = new Coordinate(gridValue.Column / 2 * grid.Size, gridValue.GetNumericRow() * grid.Size);
+            bottomRightVertex = new Coordinate((gridValue.Column / 2 + adjustPosition) * grid.Size,
+                gridValue.GetNumericRow() * grid.Size);
             return new Triangle(topLeftVertex, outerVertex, bottomRightVertex);
         }
 
         public GridValue ProcessGridValueFromTriangularShape(Grid grid, Triangle triangle)
         {
-            // TODO: Calculate the grid value.
-            return new GridValue(0, 0);
+            // highest y value tell us the row.
+            int rowLetterValue =
+                Math.Max(Math.Max(triangle.TopLeftVertex.Y, triangle.OuterVertex.Y), triangle.BottomRightVertex.Y) /
+                grid.Size;
+
+            int firstValue;
+            int secondValue;
+            // find matching X values
+            if (triangle.TopLeftVertex.X == triangle.OuterVertex.X)
+            {
+                firstValue = triangle.TopLeftVertex.X;
+                secondValue = triangle.BottomRightVertex.X;
+            }
+            else if (triangle.TopLeftVertex.X == triangle.BottomRightVertex.X)
+            {
+                firstValue = triangle.TopLeftVertex.X;
+                secondValue = triangle.OuterVertex.X;
+            }
+            else
+            {
+                firstValue = triangle.OuterVertex.X;
+                secondValue = triangle.TopLeftVertex.X;
+            }
+            var columnValue = firstValue * 2 / grid.Size;
+            
+            // if second value greater than first + 1, this happens if the column is supposed to be an odd num.
+            if (secondValue > firstValue)
+            {
+                columnValue += 1;
+            }
+
+            return new GridValue(rowLetterValue, columnValue);
         }
     }
 }
