6e9f4b525e285f6c084cb579e8fd81524652f616
Regan <46687043+Regan@users.noreply.github.com>
2022-07-03 14:39:14 +0100

    added a constructor to initialize the list of cords.

--- TechnicalTest.API/DTOs/CalculateCoordinatesResponseDTO.cs
+++ TechnicalTest.API/DTOs/CalculateCoordinatesResponseDTO.cs
@@ -3,7 +3,11 @@
     public class CalculateCoordinatesResponseDTO
     {
         public List<Coordinate> Coordinates { get; set; }
-
+        
+        public CalculateCoordinatesResponseDTO(List<Coordinate> coordinates)
+        {
+            Coordinates = coordinates;
+        }
         public class Coordinate
         {
             public int X { get; set; }
