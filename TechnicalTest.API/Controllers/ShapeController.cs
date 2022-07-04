cd0d35e4a198899a1a21c2688ce2154a349b4207
Regan <46687043+Regan@users.noreply.github.com>
2022-07-04 02:30:32 +0100

    improved regex checking for gridValue format. refactored code.

--- TechnicalTest.API/Controllers/ShapeController.cs
+++ TechnicalTest.API/Controllers/ShapeController.cs
@@ -36,51 +36,48 @@ namespace TechnicalTest.API.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [Route("CalculateCoordinates")]
         [HttpPost]
-        public IActionResult CalculateCoordinates([FromBody]CalculateCoordinatesDTO calculateCoordinatesRequest)
+        public IActionResult CalculateCoordinates([FromBody] CalculateCoordinatesDTO calculateCoordinatesRequest)
         {
             // Get the ShapeEnum and if it is default (ShapeEnum.None) or not triangle, return BadRequest as only Triangle is implemented yet.
-            var shapeEnum = (ShapeEnum)calculateCoordinatesRequest.ShapeType;
+            var shapeEnum = (ShapeEnum) calculateCoordinatesRequest.ShapeType;
             if (!shapeEnum.Equals(ShapeEnum.Triangle))
             {
                 if (shapeEnum.Equals(ShapeEnum.None) || shapeEnum.Equals(ShapeEnum.Other))
                 {
                     return BadRequest("Not yet implemented, only Triangle is supported at the moment.");
-
                 }
+
                 return BadRequest("Only Triangle, None or Other is supported.");
             }
-            
-            // checks for supported row and columns.
-            var gridValue = new GridValue(calculateCoordinatesRequest.GridValue);
-            if(!Regex.IsMatch(gridValue.Row,"[a-fA-F]"))
+
+            /*
+             checks for supported row and columns.
+             ensures that the first value is a-F and then in range of 1-12. e.g. A1, F11 valid. 11F, B13, A1A invalid.
+             */
+            if (!Regex.IsMatch(calculateCoordinatesRequest.GridValue, "^[a-fA-F]{1}([1-9]|1[012])$"))
             {
-                return BadRequest("Only a-f, A-F rows are supported.");
+                return BadRequest("Only a-f, A-F rows & 1-12 columns are supported.");
             }
 
-            if (!Enumerable.Range(1, 12).Contains(gridValue.Column))
-            {
-                return BadRequest("Only 1-12 columns are supported.");
-            }
-
-            var grid = new Grid(calculateCoordinatesRequest.Grid.Size);
-            
-            if (grid.Size != 10)
+            if (calculateCoordinatesRequest.Grid.Size != 10)
             {
                 return BadRequest("Only Grid Size 10 is currently supported.");
             }
-            
+
             //  calls the Calculate function in the shape factory.
-            var shape = _shapeFactory.CalculateCoordinates(shapeEnum, grid, gridValue);
+            var shape = _shapeFactory.CalculateCoordinates(shapeEnum, new Grid(calculateCoordinatesRequest.Grid.Size),
+                new GridValue(calculateCoordinatesRequest.GridValue));
 
             //  returns BadRequest with error message if the calculate result is null
             if (shape == null)
             {
                 return BadRequest("Calculation Result was null");
             }
-            
+
             //  uses linq query to get coordinates from shape, and then creates new DTOs to a list.
-            var coordinateList = shape.Coordinates.Select(coordinate => new CalculateCoordinatesResponseDTO.Coordinate(coordinate.X, coordinate.Y)).ToList();
-            
+            var coordinateList = shape.Coordinates.Select(coordinate =>
+                new CalculateCoordinatesResponseDTO.Coordinate(coordinate.X, coordinate.Y)).ToList();
+
             return Ok(new CalculateCoordinatesResponseDTO(coordinateList));
         }
 
@@ -98,44 +95,42 @@ namespace TechnicalTest.API.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [Route("CalculateGridValue")]
         [HttpPost]
-        public IActionResult CalculateGridValue([FromBody]CalculateGridValueDTO gridValueRequest)
+        public IActionResult CalculateGridValue([FromBody] CalculateGridValueDTO gridValueRequest)
         {
-	        // Get the ShapeEnum and if it is default (ShapeEnum.None) or not triangle, return BadRequest as only Triangle is implemented yet.
-            var shapeEnum = (ShapeEnum)gridValueRequest.ShapeType;
+            // Get the ShapeEnum and if it is default (ShapeEnum.None) or not triangle, return BadRequest as only Triangle is implemented yet.
+            var shapeEnum = (ShapeEnum) gridValueRequest.ShapeType;
             if (!shapeEnum.Equals(ShapeEnum.Triangle))
             {
                 if (shapeEnum.Equals(ShapeEnum.None) || shapeEnum.Equals(ShapeEnum.Other))
                 {
                     return BadRequest("Not yet implemented, only Triangle is supported at the moment.");
-
                 }
+
                 return BadRequest("Only Triangle, None or Other is supported.");
             }
-            
-            var grid = new Grid(gridValueRequest.Grid.Size);
 
-            if (grid.Size != 10)
+            if (gridValueRequest.Grid.Size != 10)
             {
                 return BadRequest("Only Grid Size 10 is currently supported.");
             }
-            
-            // only 60 pixels are supported.
-            var isSupportedPixelRange = gridValueRequest.Vertices.Any(vertices => vertices.x is > 60 or < 0 || vertices.y is > 60 or < 0);
 
-            if (isSupportedPixelRange)
+            // only 0-60 pixels are supported.
+            var isNotSupportedPixelRange =
+                gridValueRequest.Vertices.Any(vertices => vertices.x is > 60 or < 0 || vertices.y is > 60 or < 0);
+
+            if (isNotSupportedPixelRange)
             {
                 return BadRequest("Only 0-60 vertices (pixels) are currently supported.");
             }
 
             // LINQ expression to get vertices and create new coordinate objects and add them to a list.
-            var coordinateList = gridValueRequest.Vertices.Select(vertices => new Coordinate(vertices.x, vertices.y)).ToList();
-
-            // Create new Shape with coordinates based on the parameters from the DTO.
-            var shape = new Shape(coordinateList);
+            var coordinateList = gridValueRequest.Vertices.Select(vertices => new Coordinate(vertices.x, vertices.y))
+                .ToList();
 
             // Call the function in the shape factory to calculate grid value.
-            var gridValue = _shapeFactory.CalculateGridValue(shapeEnum, grid, shape);
-            
+            var gridValue = _shapeFactory.CalculateGridValue(shapeEnum, new Grid(gridValueRequest.Grid.Size),
+                new Shape(coordinateList));
+
             // If the GridValue result is null then return BadRequest with an error message.
 
             if (gridValue == null)
